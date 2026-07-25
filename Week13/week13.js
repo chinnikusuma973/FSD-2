9eb91b8bcdc11a0eefa37073a9fd779c9ad0c14e
chinni <bhargavasaichinni@gmail.com>
2026-07-25 21:46:04 +0530

    Added Week13 programs

---
+++ Week13/week13.js
@@ -0,0 +1,6 @@
+"use strict";
+let Name = "Kusuma";
+let Age = 20;
+let cgpa = 8.9;
+let isPlaced = false;
+console.log(Name, Age, cgpa, isPlaced);
---
+++ Week13/week13.ts
@@ -0,0 +1,6 @@
+let Name: string = "Kusuma";
+let Age: number = 20;
+let cgpa: number = 8.9;
+let isPlaced: boolean = false;
+
+console.log(Name, Age, cgpa, isPlaced);
