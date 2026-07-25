5d8fece7567e26814ac52c9010e2bc4e619fb41b
chinni <bhargavasaichinni@gmail.com>
2026-07-25 22:04:03 +0530

    Added Week23 programs

---
+++ Week23/week23.js
@@ -0,0 +1,6 @@
+"use strict";
+function power(base, exponent = 2) {
+    return base ** exponent;
+}
+console.log(power(5));
+console.log(power(5, 3));
---
+++ Week23/week23.ts
@@ -0,0 +1,6 @@
+function power(base: number, exponent: number = 2): number {
+    return base ** exponent;
+}
+
+console.log(power(5));
+console.log(power(5, 3));
